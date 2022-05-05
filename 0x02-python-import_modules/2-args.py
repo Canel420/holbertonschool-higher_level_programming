ccd40c7d78d2e4801837a036f4e8d47a7243d3cb
Canel420 <4521@holbertonschool.com>
2022-05-05 17:01:50 -0500

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    num = len(argv)
+
+    print("{:d} {:s}{:s}".format(num - 1,
+                                 "argument" if num == 2 else "arguments",
+                                 "." if num == 1 else ":"))
+
+    for key, value in enumerate(argv):
+        if key > 0:
+            print("{:d}: {:s}".format(key, value))
