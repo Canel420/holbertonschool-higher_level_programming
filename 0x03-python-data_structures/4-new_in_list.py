5fd94450c279eb9dac525c047acc06927f2b2213
Canel420 <4521@holbertonschool.com>
2022-05-09 13:31:23 -0500

    less or equal

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,4 +5,4 @@ def new_in_list(my_list, idx, element):
     my_list2 = my_list.copy()
     my_list2[idx] = element
 
-    return (my_list2 if 0 <= idx <= len(my_list) else my_list)
+    return (my_list2 if 0 <= idx < len(my_list) else my_list)
