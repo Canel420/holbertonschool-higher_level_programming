3b03170f4a7b0b5f93b83f25834caafcbf0b9bf0
Canel420 <4521@holbertonschool.com>
2022-05-09 13:17:44 -0500

    copy list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+
+    my_list2 = my_list.copy()
+    my_list2[idx] = element
+
+    return (my_list2 if 0 <= idx <= len(my_list) else my_list)
