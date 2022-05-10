d4a5a64637c51903801042e1e6e46cc0fbd56f63
Canel420 <4521@holbertonschool.com>
2022-05-09 19:09:26 -0500

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * check_Pal - Recursive function to check if the linked list is a palindrome
+ *
+ * @left: Start node of the list
+ * @right: End node of the list
+ *
+ * Description: Reaches the end of the linked list and compares if the
+ * last node has the same value as the first node and so on.
+ *
+ * Return: Return 1 if palindrome
+ *
+ */
+
+int check_Pal(listint_t **left, listint_t *right)
+{
+	int res;
+
+	if (right == NULL)
+		return (1);
+
+	res = check_Pal(left, right->next) && ((*left)->data == right->data);
+	(*left) = (*left)->next;
+
+	return (res);
+}
+
+/**
+ * is_palindrome - Function to check if the linked list is a palindrome
+ *
+ * @head: Reference to head node of list
+ *
+ * Description: Calls recursive function to check if palindrome
+ *
+ * Return: The call to the recursive function.
+ */
+
+int is_palindrome(listint_t **head)
+{
+	return (check_Pal(&head, head));
+}
