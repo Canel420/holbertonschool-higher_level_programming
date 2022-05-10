b1322200522fa4f77e4d8470762f2d78bfeffc15
Canel420 <4521@holbertonschool.com>
2022-05-10 16:58:06 -0500

    whitespace

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,7 +3,7 @@ def weight_average(my_list=[]):
 
     if not my_list:
         return (0)
-   
+
     numerator = sum([score*weight for (score, weight) in my_list])
     denominator = sum([weight for (score, weight) in my_list])
     return (numerator / denominator)
