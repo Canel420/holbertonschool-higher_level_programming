60733df0666ddcb2ea22c08e111cda3d59e46be9
Canel420 <4521@holbertonschool.com>
2022-05-10 16:18:19 -0500

    romans

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+
+    if (type(roman_string) is str):
+
+        number = list(roman_string)
+        romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                  'M': 1000}
+        sum = 0
+
+        for i in number:
+            for k, v in romans.items():
+                if (i == k):
+                    sum += v
+        return sum
+    else:
+        return (0)
