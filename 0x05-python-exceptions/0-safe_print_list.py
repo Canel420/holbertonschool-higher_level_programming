157584ce52b565c62a0637577c6b43c5ad02aa23
Canel420 <4521@holbertonschool.com>
2022-05-23 12:28:50 -0500

    print x elements

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    while count < x:
+        try:
+            elm = my_list[count]
+            count += 1
+            print(elm, end="")
+        except IndexError:
+            break
+    print()
+    return (count)
