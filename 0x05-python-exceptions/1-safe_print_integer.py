3cfb079e770a38ce0022d66e72dbeda26915296f
Canel420 <4521@holbertonschool.com>
2022-05-23 13:55:56 -0500

    typerror added

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         val = int(value)
-    except ValueError:
+    except (ValueError, TypeError):
         return False
     else:
         print("{:d}".format(val))
