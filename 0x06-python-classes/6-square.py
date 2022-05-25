94ba62e058f4631ab85976560594837e730f827b
Canel420 <4521@holbertonschool.com>
2022-05-24 20:34:32 -0500

    executables

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -104,11 +104,11 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for i in range(self.position[1]):
+            for i in range(self.__position[1]):
                 print()
-            for i in range(self.size):
-                for k in range(self.position[0]):
-                    print(" ")
-                for j in range(self.size):
+            for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print(end=" ")
+                for j in range(self.__size):
                     print("#", end='')
                 print()
