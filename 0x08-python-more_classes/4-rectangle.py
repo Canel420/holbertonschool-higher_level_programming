fe4fa32e0383866074d9cee6c5704729f1a72c91
Canel420 <4521@holbertonschool.com>
2022-05-31 19:08:26 -0500

    comments

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -96,4 +96,7 @@ class Rectangle():
         return string
 
     def __repr__(self):
+        """
+        Official representation of the rectangle.
+        """
         return ("Rectangle ({:d}, {:d})".format(self.__width, self.__height))
