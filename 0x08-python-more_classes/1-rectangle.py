a60186b12155aea66c1d36142f8d6f6b2675783f
Canel420 <4521@holbertonschool.com>
2022-05-31 19:28:35 -0500

    else added

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -29,28 +29,30 @@ class Rectangle:
 
     @property
     def height(self):
-        """ Get height """
+        """ Getter for the private attribute height """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Set height """
+        """ Settter for the private attribute height """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     @property
     def width(self):
-        """ Get width """
+        """ Getter for the private attribute width """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Set width """
+        """ Setter for the private attribute width """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
