dfe2b4c51b2d10ff1431bb6e5a2af3f447e5e257
Canel420 <4521@holbertonschool.com>
2022-05-31 21:28:10 -0500

    private

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -45,8 +45,8 @@ class Rectangle:
         heigth: int
             height of the rectangle.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     def __del__(self):
