3e7e42d96e5175075b88511e3b2b6b0373c70c69
Canel420 <4521@holbertonschool.com>
2022-05-31 19:58:46 -0500

    destructor

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 
 class Rectangle:
     """
-    Rectangle Class
+    Representation of rectangle.
 
     Attributes
     ----------
@@ -37,10 +37,9 @@ class Rectangle:
         """ Settter for the private attribute height """
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     @property
     def width(self):
@@ -52,7 +51,6 @@ class Rectangle:
         """ Setter for the private attribute width """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
