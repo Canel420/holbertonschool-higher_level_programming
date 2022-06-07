02770c0a7f540ae7a7458f36d02f42d9523a91dc
Canel420 <4521@holbertonschool.com>
2022-06-07 13:00:49 -0500

    lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" lookup function """
+
+
+def lookup(obj):
+    """
+    Returns the list of available
+    attributes and methods of an object.
+    """
+    return dir(obj)
