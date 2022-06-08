4e8b698c7240d5adb01e53149203edeca20e69a4
Canel420 <4521@holbertonschool.com>
2022-06-08 18:53:40 -0500

    trailing

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """ Function to_json_string """
-
 import json
 
 
 def to_json_string(my_obj):
     """
-    Returns the JSON representation of 
+    Returns the JSON representation of
     an object.
     """
     return json.dumps(my_obj)
