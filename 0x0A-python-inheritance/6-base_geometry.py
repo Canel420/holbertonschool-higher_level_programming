f24c02138dff41d0223a495a4132a131df6f9e46
Canel420 <4521@holbertonschool.com>
2022-06-07 20:14:18 -0500

    no area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" Class BaseGeometry """
+
+
+class BaseGeometry:
+    """
+    Class with public instance method.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
