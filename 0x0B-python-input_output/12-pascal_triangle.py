1b03b11d9d87800aea9dd3ef96a01d6f2f0ea23a
Canel420 <4521@holbertonschool.com>
2022-06-08 21:36:03 -0500

    pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+""" Function pascal_triangle """
+
+
+def pascal_triangle(n):
+    """
+    Pascal triangle recomputing line
+    right to left.
+
+    Parameters
+    ----------
+    n: number of rows
+    """
+    answer = []
+
+    for row in range(n):
+        answer.append(1)  # both widen the row and initialize last element
+
+        for i in range(row - 1, 0, -1):  # fill in the row, right to left
+            answer[i] += answer[i - 1]  # current computed from previous
+
+        print(*answer)
