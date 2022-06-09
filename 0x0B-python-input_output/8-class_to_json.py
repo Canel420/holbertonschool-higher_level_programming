eca0579bf1ae9291f3203c04ea0e8fa79e92df27
Canel420 <4521@holbertonschool.com>
2022-06-08 19:10:12 -0500

    object

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+""" Function class_to_json """
 
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description
+    with simple data structure.
+    """
+    return obj.__dict__
