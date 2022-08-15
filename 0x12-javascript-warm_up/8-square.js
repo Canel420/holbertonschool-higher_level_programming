d63aeb6b4f319f334d1ba416b269a6ce9fa43d36
Canel420 <4521@holbertonstudents.com>
2022-08-15 15:57:17 -0500

    0 - 9

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const size = process.argv[2];
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    let str = '';
+    for (let j = 0; j < size; j++) {
+      str += 'x';
+    }
+    console.log(str);
+  }
+}
