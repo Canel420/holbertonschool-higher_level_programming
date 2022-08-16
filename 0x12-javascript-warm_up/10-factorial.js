1fa0a0a478b0c0f29b3a48854dec910273869819
Canel420 <4521@holbertonstudents.com>
2022-08-15 20:46:11 -0500

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,15 @@
 #!/usr/bin/node
 
+const num = process.argv[2];
+
+function factorial (a) {
+  if (isNaN(a) || a === 0) {
+    return 1;
+  } else if (a < 0) {
+    return -1;
+  } else {
+    return (a * factorial(a - 1));
+  }
+}
+
+console.log(factorial(num));
