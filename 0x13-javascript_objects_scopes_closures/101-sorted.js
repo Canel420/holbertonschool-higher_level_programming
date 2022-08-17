bb3c367b02116a252f33264957332e14b3627140
Canel420 <4521@holbertonstudents.com>
2022-08-16 19:54:22 -0500

    dict reverse

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const dict = require('./101-data').dict;
+const dict2 = {};
+
+for (element in dict) {
+  if (!dict2[dict[element]]) {  
+    dict2[dict[element]] = []; 
+  }
+  dict2[dict[element]].push(element);
+}
+
+console.log(dict2);
