f7007df4234956d021a18c899db0fe3dc20fe945
Canel420 <4521@holbertonstudents.com>
2022-08-17 12:56:11 -0500

    axios get

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-const { error } = require('console');
-
 const axios = require('axios').default;
 const URL = process.argv[2];
 
 axios.get(URL)
   .then(function (response) {
-    console.log("code: " + response.status);
+    console.log('code: ' + response.status);
   })
   .catch(function (err) {
-    console.error("code: " + err.response.status);
+    console.error('code: ' + err.response.status);
   });
