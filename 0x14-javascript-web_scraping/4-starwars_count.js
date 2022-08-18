1df66ad7d1b43ecd600779b6032cee8172eb3176
Canel420 <4521@holbertonstudents.com>
2022-08-17 21:23:04 -0500

    semistandard fixed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,17 +3,17 @@
 const axios = require('axios').default;
 const URL = process.argv[2];
 
-async function getCharacter() {
+async function getCharacter () {
   try {
     const response = await axios.get(URL);
     const data = response.data.results;
     const characters = [];
-    for(let i = 0; i < data.length; i++) {
-      characters.push(data[i]['characters']);
+    for (let i = 0; i < data.length; i++) {
+      characters.push(data[i].characters);
     }
     let count = 0;
     for (let j = 0; j < characters.length; j++) {
-      for (let k= 0; k < characters[j].length; k++) {
+      for (let k = 0; k < characters[j].length; k++) {
         if (characters[j][k].includes('18')) {
           count++;
         }
