5e416b86674f6093880ed7100515982baff0bd4c
Canel420 <4521@holbertonstudents.com>
2022-09-04 14:31:37 -0500

    headers

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -11,5 +11,5 @@ from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    response = requests.head(url)
+    response = requests.get(url)
     print(response.headers.get('X-Request-Id'))
