611a6c761ab85e485e11eb9fa760492fb1cccdf3
Canel420 <4521@holbertonstudents.com>
2022-09-04 15:56:57 -0500

    json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Script that takes in a letter and sends
+a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        data = {'q': argv[1]}
+    else:
+        data = {'q': ''}
+    response = requests.post(url, data)
+    try:
+        json_body = response.json()
+        if len(json_body) == 0:
+            print('No result')
+        else:
+            print('[{}] {}'.format(json_body['id'], json_body['name']))
+    except requests.exception.InvalidJSONError:
+        print('Not a valid JSON')
