7b7f490819b5f95ddc9cee3f44a8a1a9b11f42a9
Canel420 <4521@holbertonstudents.com>
2022-09-04 15:05:20 -0500

    error handling

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Script that takes in a URL, sends a
+request to the URL and displays the body
+of the response.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    response = requests.get(url)
+    if response.status_code < 400:
+        print(response.text)
+    else:
+        print('Error code: {}'.format(response.status_code))
