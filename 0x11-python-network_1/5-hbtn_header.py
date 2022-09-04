ac4d34d0cbe286ac54c5d5b3388feb50d2d94b6c
Canel420 <4521@holbertonstudents.com>
2022-09-04 14:24:48 -0500

    requests is awesome

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Script that takes in a URL, sends
+a request to the URL and displays the
+value of the variable X-Request-Id
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    response = requests.head(url)
+    print(response.headers['X-Request-Id'])
