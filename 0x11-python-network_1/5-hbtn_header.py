685dc695a2066ad1f9e985778cb238bba481eb33
Canel420 <4521@holbertonstudents.com>
2022-09-04 14:28:58 -0500

    requests is awesome

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -12,5 +12,4 @@ from sys import argv
 if __name__ == "__main__":
     url = argv[1]
     response = requests.head(url)
-    header = response.headers['X-Request-Id']
-    print(header)
+    print(response.headers.get('X-Request-Id'))
