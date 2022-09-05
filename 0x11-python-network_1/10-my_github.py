25f4c01b15c7772bf52e888419ec9f450b665c73
Canel420 <4521@holbertonstudents.com>
2022-09-05 08:40:23 -0500

    remove print and try-except

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Script that takes in a letter and sends
-a POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
+Script that takes your GitHub credentials
+(username and password) and uses the GitHub API
+to display your id.
 """
 
 import requests
-from requests.auth import HTTPBasicAuth
 from sys import argv
 
 if __name__ == "__main__":
     url = 'https://api.github.com/user'
     username = argv[1]
     password = argv[2]
-    response = requests.get(url, auth=HTTPBasicAuth(username, password))
-    print(response)
-    try:
-        print(response.json().get('id'))
-    except ValueError:
-        print(None)
+    response = requests.get(url, auth=(username, password))
+    print(response.json().get('id'))
